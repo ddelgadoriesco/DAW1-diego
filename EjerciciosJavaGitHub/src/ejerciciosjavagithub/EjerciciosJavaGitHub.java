9186a4990b812c1133a3b00f668100f53e2410ee
RYZEN 5 2600X <diego.delgado@alumnoshipatia.es>
2021-01-17 13:24:19 +0100

    Prueba Git

---
+++ EjerciciosJavaGitHub/src/ejerciciosjavagithub/EjerciciosJavaGitHub.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosjavagithub;
+
+/**
+ *
+ * @author RYZEN 5 2600X
+ */
+public class EjerciciosJavaGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n, m, s;
+        n=20;
+        m=30;
+        s=n+m;
+        System.out.println(s);
+        
+    }
+    
+}
